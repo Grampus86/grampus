b1db3b3a838f8fa4f7f6655e4cff891768d57945
高橋宏太 <>
2022-01-16 17:50:49 +0900

    mainファイルを修正

--- plot_3d/main_plot_3d.py
+++ plot_3d/main_plot_3d.py
@@ -7,7 +7,6 @@
 '_sample_3'はシート名が'_'から始まるため無視される.
 """
 
-import pandas as pd
 from grampus.sci_toolkits import get_interpo_data
 from grampus.pd_toolkits import Process_data_flame
 from grampus.plt_toolkits import Plot3d
@@ -36,21 +35,21 @@ dpi = 300  # 図の解像度
 font_name = 'Times New Roman'  # フォント
 
 """
-処理内容
+メイン処理
 """
-save_dir_name = \
-    OsProc.creat_dir('.', 'figure', is_append_datetime=True, is_return_dirname=True)  # 図保存用ディレクトリを保存
-pdf = Process_data_flame()  # インスタンス生成
-file_name_list = pdf.get_excel_sheet_name(file_name)  # エクセルシート名取得
-for sheetname in file_name_list:
-    df = pd.read_excel(file_name, sheet_name=sheetname, index_col=0)  # 各シートのデータを読み込みdfへ
-    x_array, y_array, z_array = pdf.to_xyz_array(df)  # dfをx,y,z配列へ
-    xx_array, yy_array, zz_array = get_interpo_data(x_array, y_array, z_array, x_mesh=x_mesh, y_mesh=y_mesh)  # 線形補間
-    plc = Plot3d.set_ax(figsize=figsize)  # カラーマップ用インスタンス生成
-    plc.set_plot_layout(grid_axis='none')  # レイアウト設定
-    plc.set_aspect()  # アスペクト比
-    plc.set_axis_range(x_min=x_axis_min, x_max=x_axis_max, y_min=y_axis_min, y_max=y_axis_max)  # xy軸の範囲設定
-    plc.plot_color_map(xx_array, yy_array, zz_array, set_zmin=z_axis_min, set_zmax=z_axis_max)  # カラーマップ
-    plc.set_axis_label(xlabel=xlabel, ylabel=ylabel, font_name=font_name)  # xy軸ラベル
-    plc.savefig(sheetname, dpi=dpi, is_append_datetime=True, save_dir_name=save_dir_name)  # 図保存
-print('Done!')
+if __name__ == '__main__':
+    save_dir_name = OsProc.creat_dir('.', 'figure', is_append_datetime=True, is_return_dirname=True)  # 図保存用ディレクトリを保存
+    pdf = Process_data_flame()  # インスタンス生成
+    file_name_list = pdf.get_excel_sheet_name(file_name)  # エクセルシート名取得
+    for sheetname in file_name_list:
+        df = pdf.read_data_file(file_name, sheet_name=sheetname, index_col=0)
+        x_array, y_array, z_array = pdf.to_xyz_array(df)  # dfをx,y,z配列へ
+        xx_array, yy_array, zz_array = get_interpo_data(x_array, y_array, z_array, x_mesh=x_mesh, y_mesh=y_mesh)  # 線形補間
+        plc = Plot3d.set_ax(figsize=figsize)  # カラーマップ用インスタンス生成
+        plc.set_plot_layout(grid_axis='none')  # レイアウト設定
+        plc.set_aspect()  # アスペクト比
+        plc.set_axis_range(x_min=x_axis_min, x_max=x_axis_max, y_min=y_axis_min, y_max=y_axis_max)  # xy軸の範囲設定
+        plc.plot_color_map(xx_array, yy_array, zz_array, set_zmin=z_axis_min, set_zmax=z_axis_max)  # カラーマップ
+        plc.set_axis_label(xlabel=xlabel, ylabel=ylabel, font_name=font_name)  # xy軸ラベル
+        plc.savefig(sheetname, dpi=dpi, is_append_datetime=True, save_dir_name=save_dir_name)  # 図保存
+    print('Done!')
